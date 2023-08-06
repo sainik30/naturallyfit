b6d79a47e7e7a747cfe0ff07f20960192556d719
sainik <writetosainik@gmail.com>
2023-08-06 08:01:56 -0700

    adding some updates

--- supplement.py
+++ supplement.py
@@ -4,12 +4,32 @@ import openai
 
 #def api(prompt):
 def get_remedies(user_input):
+    prompts = []
     response = []
-    response1 = 'Best 5 useful Natural food items for ' + user_input
-    response2 = 'Best 5 useful Yoga for ' + user_input
-    response3 = 'Best 5 useful Ayurvedic remedies for ' + user_input
-    response.append(response1)
-    response.append(response2)
-    response.append(response3)
+    prompt1 = 'Best 5 useful Natural food items for ' + user_input
+    prompt2 = 'Best 5 useful Yoga for ' + user_input
+    prompt3 = 'Best 5 useful Ayurvedic supplements for ' + user_input
+    prompts.append(prompt1)
+    prompts.append(prompt2)
+    prompts.append(prompt3)
 
+    openai.api_key = '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'
+    messages = [{"role": "system", "content": "You are an intelligent assistant."}]
+
+    for i in range(len(prompts)):
+        message = prompts[i]
+
+    # message = f'Please write a remedy for {}'
+        if message:
+            messages.append(
+                {"role": "user", "content": message},
+            )
+            chat = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo", messages=messages
+            )
+        reply = chat.choices[0].message.content
+        response.append(reply)
+
+   
     return response
+
