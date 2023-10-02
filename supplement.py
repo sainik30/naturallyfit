f5226d309a39ba1f8ed7ff56efb1ce11c7f25754
sainik <writetosainik@gmail.com>
2023-10-02 09:51:03 -0700

    Added os

--- supplement.py
+++ supplement.py
@@ -1,12 +1,14 @@
 import requests
 import openai
+import os
 
 #def api(prompt):
 def get_remedies(user_input):
     prompts = []
     response = []
     prompt = "Remedies, natural healthy food, yoga exercise for " + user_input +  ". Give the output in 3 separate blocks of paragraph with points in each paragraph."
-
+    
+    key = os.getenv(API_key, default = None)
     openai.api_key = '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'
     messages = [{"role": "system", "content": "You are an intelligent assistant."}]
 
