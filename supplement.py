a1010a71d11596af73b85e3368c4bd6490e693fd
sainik <writetosainik@gmail.com>
2023-10-08 08:34:36 -0700

    changed API key

--- supplement.py
+++ supplement.py
@@ -1,15 +1,17 @@
 import requests
 import openai
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
+key = os.environ.get("API_KEY")
 #def api(prompt):
 def get_remedies(user_input):
     prompts = []
     response = []
     prompt = "Remedies, natural healthy food, yoga exercise for " + user_input +  ". Give the output in 3 separate blocks of paragraph with points in each paragraph."
 
-    #API_key = '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'
-    openai.api_key = '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'
+    openai.api_key = key 
     messages = [{"role": "system", "content": "You are an intelligent assistant."}]
 
     # for i in range(len(prompts)):
